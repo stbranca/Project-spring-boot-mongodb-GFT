03b1af21cc85debbe7f76c62c5cc17d0a5c23640
stbranca <eldermelobatista@gmail.com>
2022-03-01 11:20:53 -0300

    Conectando ao Mongodb com Repository e Service

---
+++ src/main/java/com/eldermelo/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.eldermelo.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.eldermelo.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
--- src/main/java/com/eldermelo/workshopmongo/services/UserService.java
+++ src/main/java/com/eldermelo/workshopmongo/services/UserService.java
@@ -1,5 +1,22 @@
 package com.eldermelo.workshopmongo.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.eldermelo.workshopmongo.domain.User;
+import com.eldermelo.workshopmongo.repository.UserRepository;
+
+@Service
 public class UserService {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll(){
+		return repo.findAll();
+		
+	}
 
 }
