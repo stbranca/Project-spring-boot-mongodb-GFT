3e4a73dc13a0db61c5507b33703b66121431af3d
stbranca <eldermelobatista@gmail.com>
2022-03-02 20:54:58 -0300

    Projeção dos Dados do Autor com DTO

--- src/main/java/com/eldermelo/workshopmongo/config/Instantiation.java
+++ src/main/java/com/eldermelo/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.eldermelo.workshopmongo.domain.Post;
 import com.eldermelo.workshopmongo.domain.User;
+import com.eldermelo.workshopmongo.dto.AuthorDTO;
 import com.eldermelo.workshopmongo.repository.PostRepository;
 import com.eldermelo.workshopmongo.repository.UserRepository;
 
@@ -35,10 +36,12 @@ private PostRepository PostRepository;
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("28/02/2022"), "Partiu viagem", "Vou viajar pa São Paulo, Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("02/03/2022"), "Bom dia", "Acordei feliz hoje!", maria);
-		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("28/02/2022"), "Partiu viagem", "Vou viajar pa São Paulo, Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("02/03/2022"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO( maria));
+		
+		
 		PostRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
