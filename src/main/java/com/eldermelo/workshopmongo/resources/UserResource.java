b4664b64eacdff06de21140b5ace55570c007093
stbranca <eldermelobatista@gmail.com>
2022-03-02 14:50:52 -0300

    Atualização de Usuário com Put

--- src/main/java/com/eldermelo/workshopmongo/resources/UserResource.java
+++ src/main/java/com/eldermelo/workshopmongo/resources/UserResource.java
@@ -52,4 +52,13 @@ public ResponseEntity<Void> delete(@PathVariable String id) {
 	return ResponseEntity.noContent().build();
 }
 
+@RequestMapping(value="/(id)", method=RequestMethod.PUT)
+public ResponseEntity<Void> update(@RequestBody UserDTO objDTO, @PathVariable String id) {
+	User obj = service.fromDTO(objDTO);
+	obj.setId(id);
+	obj = service.update(obj);
+	return ResponseEntity.noContent().build();
+	
+}
+
 }
