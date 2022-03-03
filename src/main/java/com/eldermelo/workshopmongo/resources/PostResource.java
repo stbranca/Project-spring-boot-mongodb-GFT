910ac20df7efa1404fd2ed364e90d6a6f0235e0c
stbranca <eldermelobatista@gmail.com>
2022-03-02 23:51:30 -0300

    Consulta Simples com Query Methods

--- src/main/java/com/eldermelo/workshopmongo/resources/PostResource.java
+++ src/main/java/com/eldermelo/workshopmongo/resources/PostResource.java
@@ -1,13 +1,17 @@
 package com.eldermelo.workshopmongo.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.eldermelo.workshopmongo.domain.Post;
+import com.eldermelo.workshopmongo.resources.util.URL;
 import com.eldermelo.workshopmongo.services.PostService;
 
 @RestController
@@ -23,5 +27,10 @@ public ResponseEntity<Post> findById(@PathVariable String id) {
 	return ResponseEntity.ok().body(obj);
 }
 
-
+@RequestMapping(value="/titlesearch", method=RequestMethod.GET)
+public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text) {
+	text = URL.decodeParam(text);
+	List<Post> list = service.findByTitle(text);
+	return ResponseEntity.ok().body(list);
+}
 }
